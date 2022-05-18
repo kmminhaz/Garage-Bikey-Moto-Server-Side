597a52d823963fc298f16caaa0edf777e5b640f8
kmminhaz <kmminhaj.khan@gmail.com>
2022-05-18 22:01:48 +0600

    retry for API inventory/:id

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
   res.send("Hello World with heroku!");
 });
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xoxwb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
